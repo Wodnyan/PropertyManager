4d91fd1b0fc4f2d638aebb444f8df06771ae433c
Albert Gergo <gergoalbert.gta@gmail.com>
2021-01-06 18:48:30 +0200

    Create properties

--- client/App.tsx
+++ client/App.tsx
@@ -1,39 +1,8 @@
 import * as React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import Home from "./views/Home";
-import { Login, SignUp } from "./views/AuthPages";
-import Choose, { TenantChoice, LandlordChoice } from "./views/Choose";
-import { Screens } from "./constants";
-import { Provider, connect } from "react-redux";
-import store from "./redux/store";
-import { ADD_USER } from "./redux/actions/user";
-
-const Stack = createStackNavigator();
+import Views from "./views/";
 
 function App() {
-  return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name={Screens.Home} component={Home} />
-          <Stack.Screen name={Screens.Login} component={Login} />
-          <Stack.Screen name={Screens.SignUp} component={SignUp} />
-          <Stack.Screen name={Screens.Choose} component={Choose} />
-          <Stack.Screen name={Screens.TenantChoice} component={TenantChoice} />
-          <Stack.Screen
-            name={Screens.LandlordChoice}
-            component={LandlordChoice}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
-  );
+  return <Views />;
 }
 
-const mapStateToProps = (state: any) => {
-  const { user } = state;
-  return { user };
-};
 export default App;
-//export default connect(mapStateToProps, { ADD_USER })(App);
